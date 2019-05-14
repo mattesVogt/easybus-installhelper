296271da86ed3edecdfdf23e78732aaac9c87615
Matthias Vogt <m.vogt@4net.de>
2019-05-14 14:44:29 +0200

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Fournet\EasybusBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Fournet\EasybusBundle\FournetEasybusBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(FournetEasybusBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
